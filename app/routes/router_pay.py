47f2e63c5c1e5c90c16c17a9355b1ca0a0ceb5e5
Phucsinh238 <muabanchotang@gmail.com>
2025-08-29 21:34:07 +0700

    Update router_pay.py

--- app/routes/router_pay.py
+++ app/routes/router_pay.py
@@ -107,7 +107,7 @@ def payment_success(request: Request, paymentId: str, PayerID: str, file_id: int
                    a.click();
                    // Sau 2 giây quay về trang chủ
                    setTimeout(function() {{
-                       window.location.href = "/";
+                       window.location.href = "https://fastapi-pdf-app.onrender.com/";
                    }}, 2000);
                </script>
            </body>
