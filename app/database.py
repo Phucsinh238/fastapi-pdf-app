0f416029753ddde196553812261a07164ae3def7
Phucsinh238 <muabanchotang@gmail.com>
2025-12-27 11:58:52 +0700

    Update database.py

--- app/database.py
+++ app/database.py
@@ -17,7 +17,15 @@ import os
 # Lấy DATABASE_URL từ biến môi trường Render
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://neondb_owner:npg_YCA1sk4JfnIF@ep-calm-king-a1bgsh11-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
 
-engine = create_engine(DATABASE_URL)
+#engine = create_engine(DATABASE_URL)
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,   # 🔥 CHECK connection trước khi dùng
+    pool_recycle=300,     # 🔥 recycle sau 5 phút
+)
+
+
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
